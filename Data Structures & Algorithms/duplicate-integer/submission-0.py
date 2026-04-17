612ed47d15cb2432c641f8433068f5b5bb2d3eaa
Tritam Moahnty <77680898+tritammohanty@users.noreply.github.com>
2026-04-17 15:10:52 +0530

    Added Question

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,3 +1,15 @@
+""" 
+Q: Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+E1: 
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+E2:
+Input: nums = [1, 2, 3, 4]
+Output: false
+"""
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         if len(set(nums)) != len(nums):
